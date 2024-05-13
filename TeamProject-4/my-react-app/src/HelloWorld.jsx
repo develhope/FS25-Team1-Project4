31ebedb196459e3b821c95f0b0984e5e0437a0d4
Henvil <turningsheeps@gmail.com>
2024-05-13 22:38:09 +0200

    initialize development

---
+++ TeamProject-4/my-react-app/src/HelloWorld.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function HelloWorld() {
+    return (
+        <div>
+            <img src="my-react-app/src/assets/backgroundImage_trasparente.png" />
+        </div>
+    );
+}
