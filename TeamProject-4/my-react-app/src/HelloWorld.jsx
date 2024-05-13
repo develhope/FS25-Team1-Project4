51d61892880bae7070685bb931d8cf7449c5d80c
Henvil <turningsheeps@gmail.com>
2024-05-13 22:44:01 +0200

    test

--- TeamProject-4/my-react-app/src/HelloWorld.jsx
+++ TeamProject-4/my-react-app/src/HelloWorld.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function HelloWorld() {
     return (
         <div>
-            <img src="my-react-app/src/assets/backgroundImage_trasparente.png" />
+            <img src="photomode_20102023_135431.png" />
         </div>
     );
 }
