f8e35ba504a94e4fc95d53046112cf32e2a148fd
Vittorio De Giorgio <vdegiorgio8@gmail.com>
2024-05-17 01:53:05 +0200

    Aggiunto hover alla Navbar

--- TeamProject-4/my-react-app/src/components/NavbarDefault.jsx
+++ TeamProject-4/my-react-app/src/components/NavbarDefault.jsx
@@ -5,19 +5,21 @@ export function NavbarDefault() {
     return (
         <div className="navbar">
             <ul>
-                <li className="hover:text-[#3ede9b] px-3 py-2 rounded-lg transition duration-300 ease-in-out">
-                    <Link to="/">Home</Link>
+                <li className="hover:text-[#034d8f] hover:bg-[#bff3e5] px-3 py-2 rounded-lg transition duration-300 ease-in-out">
+                    <Link to="/">
+                        <b>Home</b>
+                    </Link>
                 </li>
-                <li className="hover:text-[#3ede9b] px-3 py-2 rounded-lg transition duration-300 ease-in-out">
+                <li className="hover:text-[#034d8f] hover:bg-[#bff3e5] hover:font-bold px-3 py-2 rounded-lg transition duration-300 ease-in-out">
                     <Link to="/content">Content</Link>
                 </li>
-                <li className="hover:text-[#3ede9b] px-3 py-2 rounded-lg transition duration-300 ease-in-out">
+                <li className="hover:text-[#034d8f] hover:bg-[#bff3e5] hover:font-bold px-3 py-2 rounded-lg transition duration-300 ease-in-out">
                     <Link to="/my-react-app/src/pages/LoginPage.jsx">Login Pages</Link>
                 </li>
-                <li className="hover:text-[#3ede9b] px-3 py-2 rounded-lg transition duration-300 ease-in-out">
+                <li className="hover:text-[#034d8f] hover:bg-[#bff3e5] hover:font-bold px-3 py-2 rounded-lg transition duration-300 ease-in-out">
                     <Link to="/my-react-app/src/pages/Contact.jsx">Contact</Link>
                 </li>
-                <li className="hover:text-[#3ede9b] px-3 py-2 rounded-lg transition duration-300 ease-in-out">
+                <li className="hover:text-[#034d8f] hover:bg-[#bff3e5] hover:font-bold px-3 py-2 rounded-lg transition duration-300 ease-in-out">
                     <Link to="/my-react-app/src/pages/AboutAs.jsx">About us</Link>
                 </li>
             </ul>
