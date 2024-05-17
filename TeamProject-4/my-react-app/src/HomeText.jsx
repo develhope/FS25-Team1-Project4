009eadb0580fb52eacb8663ced3bcd876416316a
Vittorio De Giorgio <vdegiorgio8@gmail.com>
2024-05-17 02:12:07 +0200

    Aggiunte modifiche al testo home

--- TeamProject-4/my-react-app/src/HomeText.jsx
+++ TeamProject-4/my-react-app/src/HomeText.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 export default function HomeText() {
     return (
         <div className="helloworld-div ">
-            <h1 className="absolute top-[20rem] right-[38rem] left-[10rem] text-white font-semibold text-6xl">
+            <h1 className="absolute top-[15rem] right-[38rem] left-[10rem] text-white font-semibold text-6xl">
                 Let's Learn New Course Gain More Skills
             </h1>
-            <p className="absolute text-[#414b54] text-lg top-[30rem] right-[38rem] left-[10rem]">
+            <p className="absolute text-[#414b54] text-lg top-[24rem] right-[38rem] left-[10rem]">
                 Explore Italy’s ground-breaking research and education now. Watch this space, as eligible courses will continue to be added as they
                 become available
             </p>
