73ee250d58d0adb4d5100d4c7d62211909542d6c
Tega <tegaizu98@gmail.com>
2024-05-20 23:18:04 +0200

    contactfromadded

--- TeamProject-4/my-react-app/src/pages/Contact.jsx
+++ TeamProject-4/my-react-app/src/pages/Contact.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import BackgroundImage from '../components/BackgroundImage';
 import { NavbarDefault } from '../components/NavbarDefault';
+import ContactsForm from '../components/ContactsForm';
 
 export default function Contact() {
     return (
         <div>
             <BackgroundImage />
             <NavbarDefault />
-            <h1 className="text-3xl absolute top-[20rem] left-[40rem]"> CONTENUTO CONTACT</h1>
+            {/* <h1 className="text-3xl absolute top-[20rem] left-[40rem]"> CONTENUTO CONTACT</h1> */}
+            <ContactsForm  />
         </div>
     );
 }
