70bcd8e2d62cb744a5f3a66776ee88b51b229ba7
Vittorio De Giorgio <vdegiorgio8@gmail.com>
2024-05-31 13:22:01 +0200

    Aggiunta funzionalità admin or user per visualizzare (o meno) il button Edit

--- TeamProject-4/my-react-app/src/App.jsx
+++ TeamProject-4/my-react-app/src/App.jsx
@@ -1,8 +1,7 @@
-// App.js
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LogoPage from './components/LogoPage';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Content from './pages/Content';
 import AboutAs from './pages/AboutAs';
 import Contact from './pages/Contact';
@@ -10,28 +9,27 @@ import LoginPage from './pages/LoginPage';
 import Exercise from './pages/Exercise';
 import Lessons from './pages/Lessons';
 import Edit from './pages/Edit';
-import { AuthProvider } from './contexts/AuthContext';
-import ProtectedRoute from './contexts/ProtectedRoute';
+import { UserProvider } from './components/UserContext';
 
 function App() {
     return (
-        <AuthProvider>
+        <UserProvider>
             <Router>
                 <div className="main-content">
                     <LogoPage />
                     <Routes>
                         <Route path="/" element={<Home />} />
+                        <Route path="/content" element={<Content />} />
                         <Route path="/login" element={<LoginPage />} />
-                        <Route path="/content" element={<ProtectedRoute element={Content} />} />
-                        <Route path="/about-us" element={<AboutAs />} />
+                        <Route path="/about" element={<AboutAs />} />
                         <Route path="/contact" element={<Contact />} />
-                        <Route path="/exercise" element={<ProtectedRoute element={Exercise} />} />
-                        <Route path="/lessons" element={<ProtectedRoute element={Lessons} />} />
-                        <Route path="/edit" element={<ProtectedRoute element={Edit} />} />
+                        <Route path="/exercise" element={<Exercise />} />
+                        <Route path="/lessons" element={<Lessons />} />
+                        <Route path="/edit" element={<Edit />} />
                     </Routes>
                 </div>
             </Router>
-        </AuthProvider>
+        </UserProvider>
     );
 }
 
