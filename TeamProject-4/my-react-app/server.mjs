023c702c0454e7a60b47906744f076994c7b1bb9
Rossella Sonzogni <ross.sonzogni@gmail.com>
2024-06-17 23:26:47 +0200

    aggiunta gestione degli errori nel server

--- TeamProject-4/my-react-app/server.mjs
+++ TeamProject-4/my-react-app/server.mjs
@@ -5,7 +5,6 @@ import pgPromise from 'pg-promise';
 
 dotenv.config();
 
-
 const app = express();
 const port = process.env.PORT;
 
@@ -16,7 +15,7 @@ app.use(express.json());
 const db = pgPromise()('postgres://postgres:postgres@localhost:5432/postgres');
 
 const setupDb = async () => {
-    await db.none(` DROP TABLE IF EXISTS users`)
+    await db.none(` DROP TABLE IF EXISTS users`);
     await db.none(` 
             CREATE TABLE users (
             id SERIAL NOT NULL PRIMARY KEY,
@@ -25,52 +24,75 @@ const setupDb = async () => {
             role TEXT NOT NULL
         )
         `);
-    
-        await db.none(`INSERT INTO users (name, email, role) VALUES ('John Doe', 'john.doe@example.com', 'admin')`);
-        await db.none(`INSERT INTO users (name, email, role) VALUES ('Jane Doe', 'jane.doe@example.com', 'student')`);
-        await db.none(`INSERT INTO users (name, email, role) VALUES ('Mario Rossi','mario.rossi@example.com', 'student')`);
-        await db.none(`INSERT INTO users (name, email, role) VALUES ('Tizio Caio', 'tizio.caio@example.com', 'student')`);
-        await db.none(`INSERT INTO users (name, email, role) VALUES ('Luigi Bianchi', 'luigi.bianchi@example.com', 'student')`);
+
+    await db.none(`INSERT INTO users (name, email, role) VALUES ('John Doe', 'john.doe@example.com', 'admin')`);
+    await db.none(`INSERT INTO users (name, email, role) VALUES ('Jane Doe', 'jane.doe@example.com', 'student')`);
+    await db.none(`INSERT INTO users (name, email, role) VALUES ('Mario Rossi','mario.rossi@example.com', 'student')`);
+    await db.none(`INSERT INTO users (name, email, role) VALUES ('Tizio Caio', 'tizio.caio@example.com', 'student')`);
+    await db.none(`INSERT INTO users (name, email, role) VALUES ('Luigi Bianchi', 'luigi.bianchi@example.com', 'student')`);
 };
 
-setupDb()
+setupDb();
 
 app.get('/users', async (req, res) => {
-    const users = await db.many(`SELECT * FROM users`)
-    res.status(200).json(users);
+    try {
+        const users = await db.many(`SELECT * FROM users`);
+        res.status(200).json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: 'Bad request' });
+    }
 });
 
 app.get('/users/:id', async (req, res) => {
-    const { id } = req.params;
-    const user = await db.one(`SELECT * FROM users WHERE id=$1`, Number(id))
+    try {
+        const { id } = req.params;
+        const user = await db.one(`SELECT * FROM users WHERE id=$1`, Number(id));
 
-    res.status(200).json(user);
+        res.status(200).json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: 'Bad request' });
+    }
 });
 
 app.post('/users', (req, res) => {
-    const { name, email, role } = req.body;
-    //const newUser = { id, name, email, role };
-    db.none(`INSERT INTO users (name, email, role) VALUES ($1, $2, $3)`, [name, email, role])
+    try {
+        const { name, email, role } = req.body;
+        db.none(`INSERT INTO users (name, email, role) VALUES ($1, $2, $3)`, [name, email, role]);
 
-    res.status(201).json({ msg: 'New user created' });
+        res.status(201).json({ msg: 'New user created' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: 'Bad request' });
+    }
 });
 
 app.put('/users/:id', (req, res) => {
-    const { id } = req.params;
-    const { role } = req.body;
-    users = users.map((e) => (e.id === Number(id) ? { ...e, role } : e));
+    try {
+        const { id } = req.params;
+        const { role } = req.body;
+        users = users.map((e) => (e.id === Number(id) ? { ...e, role } : e));
 
-    res.status(200).json({ msg: 'User updated' });
+        res.status(200).json({ msg: 'User updated' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: 'Bad request' });
+    }
 });
 
 app.delete('/users/:id', (req, res) => {
-    const { id } = req.params;
-    db.none(`DELETE FROM users WHERE id=$1`, id)
+    try {
+        const { id } = req.params;
+        db.none(`DELETE FROM users WHERE id=$1`, id);
 
-    res.status(200).json({ msg: 'user deleted' });
+        res.status(200).json({ msg: 'user deleted' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: 'Bad request' });
+    }
 });
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
-
